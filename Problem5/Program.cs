4d3dc155b4651951c8c91a195ae8ee181487badb
Layth-Ibraheem <laythibraheem97@gmail.com>
2024-05-31 22:08:11 +0300

    Problem 5 Solution

---
+++ Problem5/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace Problem5
+{
+    // Write a program to ask the user to enter if he has a driver license, his age and if he has a recomendation
+    // then print "Hired" if he has a driver license and his age is greater than otherwise print "Rejected"
+    internal class Program
+    {
+        static bool ReadIfHasDriverLicense()
+        {
+            Console.WriteLine("Please enter y or yes if you have a driver license: ");
+
+            string? answer = "";
+            answer = Console.ReadLine();
+
+            if (answer == null)
+                return false;
+
+            return answer.ToLower() == "y" || answer.ToLower() == "yes";
+        }
+        static bool ReadIfHasRecomendation()
+        {
+            Console.WriteLine("Please enter y or yes if you have a recomindation: ");
+
+            string? answer = "";
+            answer = Console.ReadLine();
+
+            if (answer == null)
+                return false;
+
+            return answer.ToLower() == "y" || answer.ToLower() == "yes";
+        }
+        static int ReadAge()
+        {
+            Console.WriteLine("Please enter the age: ");
+            int age = int.Parse(Console.ReadLine() ?? "0");
+            return age;
+        }
+        static bool IsHired(bool hasDrivingLicense, int age)
+        {
+            return hasDrivingLicense && age > 21;
+        }
+
+        static void Main(string[] args)
+        {
+            if(ReadIfHasRecomendation() || IsHired(ReadIfHasDriverLicense(), ReadAge()))
+            {
+                Console.WriteLine("Congrats, You are hired");
+            }
+            else
+            {
+                Console.WriteLine("Sorry you do not satisfy this job conditions");
+            }
+        }
+    }
+}
