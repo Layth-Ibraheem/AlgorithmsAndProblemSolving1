58e0ca3b36425fbd58b5d608e4cc11e6a520acfd
Layth-Ibraheem <laythibraheem97@gmail.com>
2024-05-31 17:19:46 +0300

    Problem 2 Solution

---
+++ Problem2/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Problem2
+{
+    // Write a program that asks a user to enter the name then print it
+    internal class Program
+    {
+        static string? ReadName()
+        {
+            Console.WriteLine("Please enter your name: ");
+            return Console.ReadLine();
+        }
+        static void PrintName(string? name)
+        {
+            Console.WriteLine($"Hello {name}, Welcom");
+        }
+        static void Main(string[] args)
+        {
+            PrintName(ReadName());
+        }
+    }
+}
