c7e14eb7f32bfe74f55a2f670f43faec2782796d
Layth-Ibraheem <laythibraheem97@gmail.com>
2024-05-31 22:37:14 +0300

    Problem 7 Solution

---
+++ Problem7/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Problem7
+{
+    // Write a program that asks the user to enter a number then print the half of the number
+    internal class Program
+    {
+        static int ReadNumber()
+        {
+            do
+            {
+                Console.WriteLine("Please enter a number to calculate the half of it: ");
+
+                string? enteredNumber = Console.ReadLine();
+
+                if (int.TryParse(enteredNumber, out int number))
+                {
+                    return number;
+                }
+            } while (true);
+        }
+        static int GetTheHalfOfNumber(int number)
+        {
+            return number / 2;
+        }
+        static void Main(string[] args)
+        {
+            int number = ReadNumber();
+            Console.WriteLine($"The half of {number} is {GetTheHalfOfNumber(number)}");
+        }
+    }
+}
