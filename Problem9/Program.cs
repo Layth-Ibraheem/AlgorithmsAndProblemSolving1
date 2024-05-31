76592ee3556b94d108d351d5739a1c326cb7aaf5
Layth-Ibraheem <laythibraheem97@gmail.com>
2024-05-31 23:40:27 +0300

    Problem 9 Solution

---
+++ Problem9/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Problem9
+{
+    // Write a program that asks the user to enter three numbers then calculate there sum
+    internal class Program
+    {
+        static int ReadNumber(string message)
+        {
+            do
+            {
+                Console.WriteLine(message);
+
+                string? enteredNumber = Console.ReadLine();
+
+                if (int.TryParse(enteredNumber, out int number))
+                {
+                    return number;
+                }
+            } while (true);
+        }
+        static int GetSum(int number1, int number2, int number3)
+        {
+            return number1 + number2 + number3;
+        }
+        static void Main(string[] args)
+        {
+            int number1 = ReadNumber("Please enter the first number: ");
+            int number2 = ReadNumber("Please enter the second number: ");
+            int number3 = ReadNumber("Please enter the third number: ");
+
+            Console.WriteLine($"{number1} + {number2} + {number3} = {GetSum(number1, number2, number3)}");
+        }
+    }
+}
