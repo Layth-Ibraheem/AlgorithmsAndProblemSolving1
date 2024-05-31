99232e1422a6e4e93996b32bf33d7e683c70257c
Layth-Ibraheem <laythibraheem97@gmail.com>
2024-05-31 16:55:32 +0300

    Problem 1 Solution

--- Problem1/Program.cs
+++ Problem1/Program.cs
@@ -1,10 +1,15 @@
 ﻿namespace Problem1
 {
+    // Write a program to print your name on the screen
     internal class Program
     {
+        static void PrintName(string name)
+        {
+            Console.WriteLine($"Hello {name}, Welcom");
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            PrintName("Layth Fajer Ibraheem");
         }
     }
 }
