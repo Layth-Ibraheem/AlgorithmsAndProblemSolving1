6db77f44cde5e114f0793b829ec18db09add25c0
Layth-Ibraheem <laythibraheem97@gmail.com>
2024-05-31 23:36:30 +0300

    Problem 8 Solution

---
+++ Problem8/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Problem8
+{
+    // Write a program that asks the user to enter his mark and print "Pass" if the mark was greater then or equals 50, otherwise print "Fail"
+    internal class Program
+    {
+        static int ReadMark()
+        {
+            do
+            {
+                Console.WriteLine("Please enter your mark: ");
+
+                string? enteredNumber = Console.ReadLine();
+
+                if (int.TryParse(enteredNumber, out int number))
+                {
+                    return number;
+                }
+            } while (true);
+        }
+        static bool IsPassing(int mark)
+        {
+            return mark >= 50;
+        }
+        static void Main(string[] args)
+        {
+            if(IsPassing(ReadMark()))
+            {
+                Console.WriteLine("Pass");
+            }
+            else
+            {
+                Console.WriteLine("Fail");
+            }
+        }
+    }
+}
