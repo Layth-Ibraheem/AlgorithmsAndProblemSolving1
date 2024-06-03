a241c9ae839777c30fbf94b17235ba52c0811e25
Layth-Ibraheem <laythibraheem97@gmail.com>
2024-06-03 22:33:51 +0300

    Problem15 Solution

---
+++ Problem15/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Problem15
+{
+    internal class Program
+    {
+        // Write a program to calculate the rectangle area
+        static int ReadNumber(string message)
+        {
+            Console.WriteLine(message);
+            int result = int.Parse(Console.ReadLine() ?? "0");
+            return result;
+        }
+        static int CalculateRectangleArea(int a, int b)
+        {
+            return a * b;
+        }
+        static void Main(string[] args)
+        {
+            int a = ReadNumber("Please enter the first side height: ");
+            int b = ReadNumber("Please enter the second side height: ");
+
+            Console.WriteLine($"\nThe rectangle area that has {a}, {b} sides is {CalculateRectangleArea(a,b)}");
+
+        }
+    }
+}
