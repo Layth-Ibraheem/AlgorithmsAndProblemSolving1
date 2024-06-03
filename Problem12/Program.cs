dadffdcdcd9f7623ceb018a82f2b9b1a16837a12
Layth-Ibraheem <laythibraheem97@gmail.com>
2024-06-03 16:59:17 +0300

    Problem12 Solution

---
+++ Problem12/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Problem12
+{
+    internal class Program
+    {
+        static int ReadNumber(string message)
+        {
+            Console.WriteLine(message);
+            int result = int.Parse(Console.ReadLine() ?? "0");
+            return result;
+        }
+        static int GetMaxNumber(int number1, int number2)
+        {
+            return number1 > number2 ? number1 : number2;
+        }
+        static void Main(string[] args)
+        {
+            int number1 = ReadNumber("Enter the first number: ");
+            int number2 = ReadNumber("Enter the first number: ");
+            Console.WriteLine($"The max number between {number1} and {number2} is: {GetMaxNumber(number1, number2)}");
+        }
+    }
+}
