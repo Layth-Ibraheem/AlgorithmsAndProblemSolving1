7b4e8c99b94313e989e6344bb3b0694c311314e4
Layth-Ibraheem <laythibraheem97@gmail.com>
2024-06-03 22:25:21 +0300

    Problem14 Solution

---
+++ Problem14/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Problem14
+{
+    internal class Program
+    {
+        // Write a program that reads 2 numbers then swap them
+
+        static int ReadNumber(string message)
+        {
+            Console.WriteLine(message);
+            int result = int.Parse(Console.ReadLine() ?? "0");
+            return result;
+        }
+        static void SwapNumberMethod1(ref int number1, ref int number2)
+        {
+            number1 += number2;
+            number2 = number1 - number2;
+            number1 -= number2;
+        }
+        static void SwapNumberMethod2(ref int number1, ref int number2)
+        {
+            int temp = number1;
+            number1 = number2;
+            number2 = temp;
+        }
+        static void Main(string[] args)
+        {
+            int number1 = ReadNumber("Please enter the first number: ");
+            int number2 = ReadNumber("Please enter the first number: ");
+
+            Console.WriteLine("Befor Swap");
+            Console.WriteLine("Number1: " + number1);
+            Console.WriteLine("Number2: " + number2);
+
+            SwapNumberMethod1(ref number1, ref number2);
+            Console.WriteLine("\nAfter Swap");
+            Console.WriteLine("Number1: " + number1);
+            Console.WriteLine("Number2: " + number2);
+
+        }
+    }
+}
