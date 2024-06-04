3bd2ee0f8bc49668e9825ba0877a965118d79738
Layth-Ibraheem <laythibraheem97@gmail.com>
2024-06-04 17:42:34 +0300

    Problem17 Solution

---
+++ Problem17/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Problem17
+{
+    internal class Program
+    {
+        // Write a program to calculate triangle area then print it 
+
+        static double ReadNumber(string message)
+        {
+            Console.WriteLine(message);
+            double result = double.Parse(Console.ReadLine() ?? "0");
+            return result;
+        }
+        static double CalculateTriangleArea(double a, double h)
+        {
+
+            return (0.5 * a * h); // Area = 0.5 * a * h
+        }
+        static void Main(string[] args)
+        {
+            double a = ReadNumber("Please enter the long side of the triangle: ");
+            double h = ReadNumber("Please enter the height: ");
+
+            Console.WriteLine($"\nThe triangle area that has {h}, {a} sides is {CalculateTriangleArea(h, a)}");
+        }
+    }
+}
