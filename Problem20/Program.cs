d21a7848ad924d650e0c9889da36691b72ea9b55
Layth-Ibraheem <laythibraheem97@gmail.com>
2024-06-04 18:01:03 +0300

    Problem20 Solution

---
+++ Problem20/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Problem20
+{
+    internal class Program
+    {
+        // Write a program that caculates the circle area inscribed in a square
+        static double ReadNumber(string message)
+        {
+            Console.WriteLine(message);
+            double result = double.Parse(Console.ReadLine() ?? "0");
+            return result;
+        }
+        static double CalculateCircleAreaInscribedInSuare(double squareSide)
+        {
+            return Math.PI * Math.Pow(squareSide / 2, 2);
+        }
+        static void Main(string[] args)
+        {
+            double squareSide = ReadNumber("Please enter square side: ");
+            Console.WriteLine($"The circle area that has a square side = {squareSide} is: {CalculateCircleAreaInscribedInSuare(squareSide)}");
+        }
+    }
+}
