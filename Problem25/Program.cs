cfc9f5d38088e8386ae614c879f132051f33c132
Layth-Ibraheem <laythibraheem97@gmail.com>
2024-06-05 05:02:42 +0300

    Problem25 Solution

---
+++ Problem25/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Problem25
+{
+    internal class Program
+    {
+        // Write a program to ask the user to enter his age and if age is between 18 and 45 print 'Valid Age' else print 'Invalid Age' and re-ask the user to enter a valid age
+
+        static byte ReadNumber(string message)
+        {
+            Console.WriteLine(message);
+            byte result = byte.Parse(Console.ReadLine() ?? "0");
+            return result;
+        }
+        static bool IsNumberBetween(byte number, byte min, byte max)
+        {
+            return number >= min && number <= max;
+        }
+        static bool IsValidAge(byte age)
+        {
+            return IsNumberBetween(age, 18, 45);
+        }
+        static byte ReadAgeUntillValid()
+        {
+            byte age = ReadNumber("Please enter your age: ");
+            while (!IsValidAge(age))
+            {
+                age = ReadNumber("Invalid age, please enter a valid age: ");
+            }
+            return age;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Your age is: {ReadAgeUntillValid()}");
+        }
+    }
+}
