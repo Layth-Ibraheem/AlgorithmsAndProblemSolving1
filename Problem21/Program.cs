2fd09680d6661db8114750b70704104727fae864
Layth-Ibraheem <laythibraheem97@gmail.com>
2024-06-05 03:06:41 +0300

    Problem21 Solution

---
+++ Problem21/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Problem21
+{
+    internal class Program
+    {
+        // Write a program that caculates the circle area along the circumference
+
+        static double ReadNumber(string message)
+        {
+            Console.WriteLine(message);
+            double result = double.Parse(Console.ReadLine() ?? "0");
+            return result;
+        }
+        static double CalculateCircleAreaIAlongCircumference(double circumference)
+        {
+            return Math.Pow(circumference, 2) / (Math.PI * 4);
+        }
+        static void Main(string[] args)
+        {
+            double circumference = ReadNumber("Please enter circumference: ");
+            Console.WriteLine($"The circle area that has a circumference = {circumference} is: {CalculateCircleAreaIAlongCircumference(circumference)}");
+        }
+    }
+}
