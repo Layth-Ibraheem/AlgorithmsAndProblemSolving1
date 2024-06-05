d6e954941097ce0057a9e7ede8d2f5a45b2b19f5
Layth-Ibraheem <laythibraheem97@gmail.com>
2024-06-05 05:07:13 +0300

    Problem26 Solution

---
+++ Problem26/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Problem26
+{
+    internal class Program
+    {
+        // Write a program to print the numbers from 1 to N
+
+        static int ReadNumber(string message)
+        {
+            Console.WriteLine(message);
+            int result = int.Parse(Console.ReadLine() ?? "0");
+            return result;
+        }
+        static void PrintNumbersFrom1To(int N)
+        {
+            for (int i = 1; i <= N; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        static void Main(string[] args)
+        {
+            int N = ReadNumber("Please enter the number shich you want to print numbers from 1 untill this number: ");
+            Console.WriteLine($"\nNumbers from 1 to {N}: ");
+            PrintNumbersFrom1To(N);
+        }
+    }
+}
