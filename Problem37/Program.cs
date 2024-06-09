e44eac57093d4b19c1f7e47a8040d7b1926a32bf
Layth-Ibraheem <laythibraheem97@gmail.com>
2024-06-09 13:36:17 +0300

    Problem37 Solution

---
+++ Problem37/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Problem37
+{
+    internal class Program
+    {
+        // Write a program to read numbers from user then sum them untill the user enter -99 stop and print the sum
+        static int ReadNumber(string message)
+        {
+            Console.WriteLine(message);
+            int result = int.Parse(Console.ReadLine() ?? "0");
+            return result;
+        }
+        static int SumUntill()
+        {
+            int sum = 0;
+            int number = 0;
+            do
+            {
+                // Method1: apply it by putting the condition in the do-while loop to true
+
+                //int number = ReadNumber("Please enter the number to add: ");
+                //if (number == -99)
+                //    break;
+                //sum += number;
+
+
+                // Method2
+
+                sum += number;
+                number = ReadNumber("Please enter the number to add: ");
+
+            } while (number != -99);
+            return sum;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Ths sum is: {SumUntill()}");
+        }
+    }
+}
